fd5ae86aa9f7a93c3bbbbc51d514cf3481295d62
Kelly Hansen <kelly.hansen@hotbsoftware.com>
2025-11-03 16:50:45 -0800

    First commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const $phoneNumber = document.querySelector('.phone-number');
+$phoneNumber.textContent = '(949) 294-1912';
+
+const $emailAddress = document.querySelector('.email-address');
+$emailAddress.textContent = 'info@burkmanbuilders.com';
+
+const $instagram = document.querySelector('.instagram');
+$instagram.textContent = '@BigosLLC';
